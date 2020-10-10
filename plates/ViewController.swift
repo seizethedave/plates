d5cb6782ed730b7a18ff01a0d6b4cee9ed2ccb21
David Grant <david.grant@mixpanel.com>
2020-10-09 20:11:50 -0700

    progress on speech

--- plates/ViewController.swift
+++ plates/ViewController.swift
@@ -6,9 +6,9 @@
 //
 
 import UIKit
+import Speech
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, SFSpeechRecognizerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -19,5 +19,49 @@ class ViewController: UIViewController {
         sender.text = ""
     }
     
-}
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer()
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
+    
+    func recordAndRecognizeSpeech() {
+        let node = audioEngine.inputNode
+        //guard let node = audioEngine.inputNode else {return}
+        
+        let recordingFormat = node.outputFormat(forBus: 0)
+        
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat)
+        {
+            buffer, _ in self.request.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        } catch {
+            return print(error)
+        }
+        
+        guard let myRecognizer = SFSpeechRecognizer() else {
+            return
+        }
+        if !myRecognizer.isAvailable {
+            return
+        }
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: request, resultHandler: { result, error in
 
+            if let result = result {
+                print(result.bestTranscription.formattedString)
+                
+            } else if let error = error {
+                print(error)
+            }
+                    
+        })
+    }
+    
+    @IBAction func speakTouched(_ sender: UIButton) {
+        recordAndRecognizeSpeech()
+    }
+}
